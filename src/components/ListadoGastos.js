231d01b7b8db939064c9385be8ffadd5823d2475
FabianPineda <fban.pnd@gmail.com>
2023-12-12 11:53:50 -0600

    porcentaje, calculos y view

---
+++ src/components/ListadoGastos.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Text,View, StyleSheet } from 'react-native'
+import Gasto from './Gasto'
+
+const ListadoGastos = ({gastos, setModal, setGasto}) => {
+  return (
+    <View style={styles.contenedor} >
+      <Text style={styles.titulo} >Gastos</Text>
+      { 
+        gastos.length === 0 ? 
+          <Text style={styles.noGastos} >No hay gastos</Text> : 
+          gastos.map(gasto => (
+            <Gasto key={gasto.id} gasto={gasto} setModal={setModal} setGasto={setGasto} />
+          
+          ) )
+      }
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    contenedor : {
+        marginBottom : 100
+    },
+    titulo : {
+        color : '#64748B',
+        fontSize : 30,
+        textAlign : 'center',
+        fontWeight : '700',
+        marginTop : 20
+    },
+    noGastos : {
+      marginTop : 20,
+      textAlign : 'center',
+      fontSize : 20
+    }
+})
+
+export default ListadoGastos
